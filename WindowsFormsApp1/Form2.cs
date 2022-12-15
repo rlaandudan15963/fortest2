55ad2b51fb840a89318086800880db7e369ffb4d
무영 김 <limm123@daum.net>
2022-12-16 00:32:21 +0900

    초기 커밋.

---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Image image = new Bitmap(@"C:\Users\limm1\Desktop\석양.jpg");
+            this.BackgroundImage = image;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            Image image = new Bitmap(@"C:\Users\limm1\Desktop\2-2학기\3D프린팅\KakaoTalk_20221110_150049076.jpg");
+            this.BackgroundImage = image;
+        }
+    }
+}
